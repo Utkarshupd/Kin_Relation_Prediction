5488d3f6f4260c282811d70272e895a5590c70d7
Utkarsh Prakash Dixit <utkarshupd@gmail.com>
2019-11-15 12:51:15 +0530

    Update train.py

--- train.py
+++ train.py
@@ -152,65 +152,71 @@ for i in valx:
     valx=i
     break
 
-#this model takes four inputs
-input_1 = Input(shape=(IMG_SIZE_FN, IMG_SIZE_FN, 3))        #facenet for Image 1
-input_2 = Input(shape=(IMG_SIZE_FN, IMG_SIZE_FN, 3))        #facenet for image 2
-input_3 = Input(shape=(IMG_SIZE_VGG, IMG_SIZE_VGG, 3))      #VGG for image 1
-input_4 = Input(shape=(IMG_SIZE_VGG, IMG_SIZE_VGG, 3))      #VGG for image 2
+def model():
+    
+    #this model takes four inputs
+    input_1 = Input(shape=(IMG_SIZE_FN, IMG_SIZE_FN, 3))        #facenet for Image 1
+    input_2 = Input(shape=(IMG_SIZE_FN, IMG_SIZE_FN, 3))        #facenet for image 2
+    input_3 = Input(shape=(IMG_SIZE_VGG, IMG_SIZE_VGG, 3))      #VGG for image 1
+    input_4 = Input(shape=(IMG_SIZE_VGG, IMG_SIZE_VGG, 3))      #VGG for image 2
 
-fn_1 = facenet_model(input_1)
-fn_2 = facenet_model(input_2)
-vgg_1 = vgg_model(input_3)
-vgg_2 = vgg_model(input_4)
-    
-x1 = Reshape((1, 1 ,128))(fn_1)   #reshaping image array for global max pool layer
-x2 = Reshape((1, 1 ,128))(fn_2)
-x1 = Concatenate(axis=-1)([GlobalMaxPool2D()(x1), GlobalAvgPool2D()(x1)])
-x2 = Concatenate(axis=-1)([GlobalMaxPool2D()(x2), GlobalAvgPool2D()(x2)])
+    fn_1 = facenet_model(input_1)
+    fn_2 = facenet_model(input_2)
+    vgg_1 = vgg_model(input_3)
+    vgg_2 = vgg_model(input_4)
 
-#For simple, stateless custom operations, we can use lambda layers
-#the below 4 lamda functions will calcluate the square of each input image
-lambda_1 = Lambda(lambda tensor  : K.square(tensor))(fn_1) 
-lambda_2 = Lambda(lambda tensor  : K.square(tensor))(fn_2)
-lambda_3 = Lambda(lambda tensor  : K.square(tensor))(vgg_1)
-lambda_4 = Lambda(lambda tensor  : K.square(tensor))(vgg_2)
-    
-added_facenet = Add()([x1, x2])     #this function will add two images image 1 image 2 given by facenet architecture
-added_vgg = Add()([vgg_1, vgg_2])    #this function will add two images image 3 image 4 given by VGG architecture
-subtract_fn = Subtract()([x1,x2])    #this function will subtract two images image 1 image 2 given by facenet architecture
-subtract_vgg = Subtract()([vgg_1,vgg_2])   #this function will subtract two images image 3 image 4 given by VGG architecture
-subtract_fn2 = Subtract()([x2,x1])    #this function will subtract two images image 2 image 1 given by facenet architecture
-subtract_vgg2 = Subtract()([vgg_2,vgg_1])   #this function will subtract two images image 4 image 3 given by VGG architecture
-prduct_fn1 = Multiply()([x1,x2])    #this function will multiply two images image 1 image 2 given by facenet architecture
-prduct_vgg1 = Multiply()([vgg_1,vgg_2])   #this function will multiply two images image 3 image 4 given by VGG architecture
-sqrt_fn1 = Add()([lambda_1,lambda_2])        # this function implements x1^2 + x2^2 where x1 and x2 are image by facenet
-sqrt_vgg1 = Add()([lambda_3,lambda_4])       # this function implements vgg_1^2 + vgg_2^2 where vgg_1 and vgg_2 are image by VGG
-sqrt_fn2 = Lambda(lambda tensor  : K.sign(tensor)*K.sqrt(K.abs(tensor)+1e-9))(prduct_fn1) #squre_root of sqrt_fn1
-sqrt_vgg2 = Lambda(lambda tensor  : K.sign(tensor)*K.sqrt(K.abs(tensor)+1e-9))(prduct_vgg1) #squre_root of sqrt_vgg1
+    x1 = Reshape((1, 1 ,128))(fn_1)   #reshaping image array for global max pool layer
+    x2 = Reshape((1, 1 ,128))(fn_2)
+    x1 = Concatenate(axis=-1)([GlobalMaxPool2D()(x1), GlobalAvgPool2D()(x1)])
+    x2 = Concatenate(axis=-1)([GlobalMaxPool2D()(x2), GlobalAvgPool2D()(x2)])
 
-    
-added_vgg = Conv2D(128 , [1,1] )(added_vgg)
-subtract_vgg = Conv2D(128 , [1,1] )(subtract_vgg)
-subtract_vgg2 = Conv2D(128 , [1,1] )(subtract_vgg2)
-prduct_vgg1 = Conv2D(128 , [1,1] )(prduct_vgg1)
-sqrt_vgg1 = Conv2D(128 , [1,1] )(sqrt_vgg1)
-sqrt_vgg2 = Conv2D(128 , [1,1] )(sqrt_vgg2)
-    
-#finally concatenating all the above featues for final layer which is to be inputed to the dense layers.
-concatenated= Concatenate(axis=-1)([Flatten()(added_vgg), (added_facenet), Flatten()(subtract_vgg), (subtract_fn),
-                                   Flatten()(subtract_vgg2), (subtract_fn2), Flatten()(prduct_vgg1), (prduct_fn1), 
-                                   Flatten()(sqrt_vgg1), (sqrt_fn1), Flatten()(sqrt_vgg2), (sqrt_fn2)])
-    
-concatenated= Dense(500, activation="relu")(concatenated)
-concatenated= Dropout(0.1)(concatenated)
-concatenated= Dense(100, activation="relu")(concatenated)
-concatenated= Dropout(0.1)(concatenated)
-concatenated= Dense(25, activation="relu")(concatenated)
-concatenated= Dropout(0.1)(concatenated)
-out = Dense(1, activation="sigmoid")(concatenated) #output sigmoid layer
+    #For simple, stateless custom operations, we can use lambda layers
+    #the below 4 lamda functions will calcluate the square of each input image
+    lambda_1 = Lambda(lambda tensor  : K.square(tensor))(fn_1) 
+    lambda_2 = Lambda(lambda tensor  : K.square(tensor))(fn_2)
+    lambda_3 = Lambda(lambda tensor  : K.square(tensor))(vgg_1)
+    lambda_4 = Lambda(lambda tensor  : K.square(tensor))(vgg_2)
 
-#defining the model
-model = Model([input_1, input_2, input_3, input_4], out)
+    added_facenet = Add()([x1, x2])     #this function will add two images image 1 image 2 given by facenet architecture
+    added_vgg = Add()([vgg_1, vgg_2])    #this function will add two images image 3 image 4 given by VGG architecture
+    subtract_fn = Subtract()([x1,x2])    #this function will subtract two images image 1 image 2 given by facenet architecture
+    subtract_vgg = Subtract()([vgg_1,vgg_2])   #this function will subtract two images image 3 image 4 given by VGG architecture
+    subtract_fn2 = Subtract()([x2,x1])    #this function will subtract two images image 2 image 1 given by facenet architecture
+    subtract_vgg2 = Subtract()([vgg_2,vgg_1])   #this function will subtract two images image 4 image 3 given by VGG architecture
+    prduct_fn1 = Multiply()([x1,x2])    #this function will multiply two images image 1 image 2 given by facenet architecture
+    prduct_vgg1 = Multiply()([vgg_1,vgg_2])   #this function will multiply two images image 3 image 4 given by VGG architecture
+    sqrt_fn1 = Add()([lambda_1,lambda_2])        # this function implements x1^2 + x2^2 where x1 and x2 are image by facenet
+    sqrt_vgg1 = Add()([lambda_3,lambda_4])       # this function implements vgg_1^2 + vgg_2^2 where vgg_1 and vgg_2 are image by VGG
+    sqrt_fn2 = Lambda(lambda tensor  : K.sign(tensor)*K.sqrt(K.abs(tensor)+1e-9))(prduct_fn1) #squre_root of sqrt_fn1
+    sqrt_vgg2 = Lambda(lambda tensor  : K.sign(tensor)*K.sqrt(K.abs(tensor)+1e-9))(prduct_vgg1) #squre_root of sqrt_vgg1
+
+
+    added_vgg = Conv2D(128 , [1,1] )(added_vgg)
+    subtract_vgg = Conv2D(128 , [1,1] )(subtract_vgg)
+    subtract_vgg2 = Conv2D(128 , [1,1] )(subtract_vgg2)
+    prduct_vgg1 = Conv2D(128 , [1,1] )(prduct_vgg1)
+    sqrt_vgg1 = Conv2D(128 , [1,1] )(sqrt_vgg1)
+    sqrt_vgg2 = Conv2D(128 , [1,1] )(sqrt_vgg2)
+
+    #finally concatenating all the above featues for final layer which is to be inputed to the dense layers.
+    concatenated= Concatenate(axis=-1)([Flatten()(added_vgg), (added_facenet), Flatten()(subtract_vgg), (subtract_fn),
+                                       Flatten()(subtract_vgg2), (subtract_fn2), Flatten()(prduct_vgg1), (prduct_fn1), 
+                                       Flatten()(sqrt_vgg1), (sqrt_fn1), Flatten()(sqrt_vgg2), (sqrt_fn2)])
+
+    concatenated= Dense(500, activation="relu")(concatenated)
+    concatenated= Dropout(0.1)(concatenated)
+    concatenated= Dense(100, activation="relu")(concatenated)
+    concatenated= Dropout(0.1)(concatenated)
+    concatenated= Dense(25, activation="relu")(concatenated)
+    concatenated= Dropout(0.1)(concatenated)
+    out = Dense(1, activation="sigmoid")(concatenated) #output sigmoid layer
+
+    #defining the model
+    model = Model([input_1, input_2, input_3, input_4], out)
+    
+    return model
+
+model = model()
 
 model.compile(loss="binary_crossentropy", metrics=[auc], optimizer=Adam(1e-5))
 
